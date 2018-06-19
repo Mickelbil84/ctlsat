bff5ea0ff7eaccf7d920af1e9c2e5a2c71757bcc
mickelbil84 <mickelbil84@gmail.com>
2018-06-19 18:18:24 +0300

    Added logic operators

--- Formula.cs
+++ Formula.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 namespace CTLSAT
 {
+    public enum LogicOperator
+    {
+        EXISTS, ALL,
+        AND, OR, NOT, IMP,
+        EF, AF, EG, AG,
+        EX, AX, EU, AU, ER, AR,
+    }
+
     public class Formula
     {
         public Formula()
